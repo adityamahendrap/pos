0e1ade5f2c29f78d7ab8d6afd29c9fc7811787ad
adityamahendrap <ptadityamahendrap@gmail.com>
2023-06-28 22:31:30 +0800

    delete unused file

--- src/interfaces/IPayments.ts
+++
@@ -1,5 +0,0 @@
-export default interface IPayments {
-  name: string,
-  type: "CASH" | "DEBIT" | "CREDIT"
-  logo: string
-}
--- src/interfaces/IProduct.ts
+++
@@ -1,8 +0,0 @@
-export default interface IProduct {
-  sku: string,
-  name: string,
-  stock: number,
-  price: number,
-  categoryId: string,
-  image: string
-}
